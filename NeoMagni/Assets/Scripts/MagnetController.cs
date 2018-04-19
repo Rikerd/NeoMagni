b18d9f6369fda6c3143f3885bead578c05448b08
Rikerd <cursedmarkedsasuke@gmail.com>
2018-04-18 17:44:52 -0700

    Magnet Controller
    Basic state comparision and movement

--- NeoMagni/Assets/Scripts/MagnetController.cs
+++ NeoMagni/Assets/Scripts/MagnetController.cs
@@ -12,7 +12,8 @@ public class MagnetController : MonoBehaviour {
     private float middle = 1.4f;
     private float outer = 2.3f;
 
-    private MagnetState currentState;
+    protected MagnetState currentState;
+    private SpriteRenderer sprite;
 
     public enum MagnetState
     {
@@ -31,13 +32,59 @@ public class MagnetController : MonoBehaviour {
             middle = -middle;
             outer = -outer;
         }
+
+        sprite = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (currentState == MagnetState.Neutral)
         {
+            transform.position = new Vector2(middle, transform.position.y);
+        }
+
+        if ((currentState == MagnetState.Red && player.currentState == MagnetState.Blue) || (currentState == MagnetState.Blue && player.currentState == MagnetState.Red))
+        {
             transform.position = new Vector2(inner, transform.position.y);
         }
-	}
+
+        if ((currentState == MagnetState.Red && player.currentState == MagnetState.Red) || (currentState == MagnetState.Blue && player.currentState == MagnetState.Blue))
+        {
+            transform.position = new Vector2(outer, transform.position.y);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.A) && leftMagnet)
+        {
+            currentState = MagnetState.Red;
+            sprite.color = Color.red;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) && !leftMagnet)
+        {
+            currentState = MagnetState.Red;
+            sprite.color = Color.red;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D) && leftMagnet)
+        {
+            currentState = MagnetState.Blue;
+            sprite.color = Color.blue;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow) && !leftMagnet)
+        {
+            currentState = MagnetState.Blue;
+            sprite.color = Color.blue;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S) && leftMagnet)
+        {
+            currentState = MagnetState.Neutral;
+            sprite.color = Color.white;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && !leftMagnet)
+        {
+            currentState = MagnetState.Neutral;
+            sprite.color = Color.white;
+        }
+    }
 }

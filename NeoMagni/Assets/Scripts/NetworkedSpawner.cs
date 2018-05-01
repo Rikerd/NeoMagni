19215ead04c4c4fe4e89d26555e8279b6340bbfc
Crystal <crystal.agerton2015@gmail.com>
2018-05-01 11:21:17 -0700

    Fixed NetworkedSpawner to match ChickenSpawner

--- NeoMagni/Assets/Scripts/NetworkedSpawner.cs
+++ NeoMagni/Assets/Scripts/NetworkedSpawner.cs
@@ -11,76 +11,121 @@ public class NetworkedSpawner : NetworkBehaviour
 
     public float spawnTimer;
 
+    public GameObject basic;
+    public GameObject moverL;
+    public GameObject moverR;
+
+    private float inner = 0.65f;
+    private float middle = 2.5f;
+    private float outer = 4.8f;
+
+    private int spawns;
+
     [SyncVar]
     private int position;
-    private GameObject[] players;
 
     // Use this for initialization
     void Start()
     {
-        players = GameObject.FindGameObjectsWithTag("Player");
         spawnTimer = setSpawnTimer;
-        position = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(players.Length < 2)
-            players = GameObject.FindGameObjectsWithTag("Player");
-
         spawnTimer -= Time.deltaTime;
 
-        if (spawnTimer <= 0f && CheckPlayers())
+        if (spawnTimer <= 0f)
         {
             Birth();
-            spawnTimer = setSpawnTimer;
-        }
-    }
+            // change later
 
-    // checks if the players are still alive
-    bool CheckPlayers()
-    {
-        if (players.Length == 2)
-        {
-            if (players[0] != null && players[1] != null && players[0].activeSelf && players[1].activeSelf)
+            if (spawns > 45)
             {
-                return true;
+                spawnTimer = 1.5f;
+            }
+            else if (spawns > 15)
+            {
+                spawnTimer = 1f;
             }
             else
             {
-                return false;
+                spawnTimer = setSpawnTimer;
             }
+            Debug.Log(spawns);
         }
-        return false;
     }
 
 
     // spits out the chickens at random positions
     void Birth()
     {
+        spawns++;
+
+        int x = 12;
+        if (spawns > 30)
+        {
+            x = 7;
+        }
         if (isServer)
-            position = Random.Range(1, 6);
+            position = Random.Range(1, x);
 
         switch (position)
         {
             case 1:
-                Instantiate(chicken, new Vector3(-4.8f, transform.position.y, 0f), Quaternion.identity);
+                Object.Instantiate(basic, new Vector3(-outer, -11f, -1f), Quaternion.identity);
+                Object.Instantiate(basic, new Vector3(inner, -11f, -1f), Quaternion.identity);
                 break;
+
             case 2:
-                Instantiate(chicken, new Vector3(-2.5f, transform.position.y, 0f), Quaternion.identity);
+                Object.Instantiate(basic, new Vector3(-inner, -11f, -1f), Quaternion.identity);
+                Object.Instantiate(basic, new Vector3(outer, -11f, -1f), Quaternion.identity);
                 break;
             case 3:
-                Instantiate(chicken, new Vector3(-0.65f, transform.position.y, 0f), Quaternion.identity);
+                Object.Instantiate(basic, new Vector3(-inner, -11f, -1f), Quaternion.identity);
+                Object.Instantiate(basic, new Vector3(inner, -11f, -1f), Quaternion.identity);
                 break;
+
             case 4:
-                Instantiate(chicken, new Vector3(0.65f, transform.position.y, 0f), Quaternion.identity);
+                Object.Instantiate(basic, new Vector3(-outer, -11f, -1f), Quaternion.identity);
+                Object.Instantiate(basic, new Vector3(outer, -11f, -1f), Quaternion.identity);
                 break;
             case 5:
-                Instantiate(chicken, new Vector3(2.5f, transform.position.y, 0f), Quaternion.identity);
+                Object.Instantiate(basic, new Vector3(-middle, -11f, -1f), Quaternion.identity);
+                Object.Instantiate(basic, new Vector3(middle, -11f, -1f), Quaternion.identity);
                 break;
+
             case 6:
-                Instantiate(chicken, new Vector3(4.8f, transform.position.y, 0f), Quaternion.identity);
+                Object.Instantiate(basic, new Vector3(-outer, -11f, -1f), Quaternion.identity);
+                Object.Instantiate(basic, new Vector3(middle, -11f, -1f), Quaternion.identity);
+                break;
+            case 7:
+                Object.Instantiate(basic, new Vector3(-middle, -11f, -1f), Quaternion.identity);
+                Object.Instantiate(basic, new Vector3(outer, -11f, -1f), Quaternion.identity);
+                break;
+
+            case 8:
+                Object.Instantiate(moverL, new Vector3(-outer, -11f, -1f), Quaternion.identity);
+                Object.Instantiate(basic, new Vector3(middle, -11f, -1f), Quaternion.identity);
+                break;
+            case 9:
+                Object.Instantiate(moverR, new Vector3(outer, -11f, -1f), Quaternion.identity);
+                Object.Instantiate(basic, new Vector3(-middle, -11f, -1f), Quaternion.identity);
+                break;
+
+            case 10:
+                Object.Instantiate(moverL, new Vector3(-inner, -11f, -1f), Quaternion.identity);
+                Object.Instantiate(moverR, new Vector3(inner, -11f, -1f), Quaternion.identity);
+                break;
+
+            case 11:
+                Object.Instantiate(moverL, new Vector3(-outer, -11f, -1f), Quaternion.identity);
+                Object.Instantiate(moverR, new Vector3(outer, -11f, -1f), Quaternion.identity);
+                break;
+
+            case 12:
+                Object.Instantiate(moverL, new Vector3(-middle, -11f, -1f), Quaternion.identity);
+                Object.Instantiate(moverR, new Vector3(middle, -11f, -1f), Quaternion.identity);
                 break;
         }
     }
